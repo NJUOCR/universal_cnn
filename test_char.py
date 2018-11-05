cd5a15815fc134d55d5533a21741f095a52abf12
AssKickerSurfaceBook <121584565@163.com>
2018-11-05 18:45:37 +0800

    test char-wise accuracy

---
+++ test_char.py
@@ -0,0 +1,39 @@
+import argparse
+import json
+
+from data import SingleCharData
+from main import Main
+
+parse = argparse.ArgumentParser()
+
+parse.add_argument("--src_dir", type=str, default="/usr/local/src/data/sc/val",
+                   help='the directory containing all the char images to be tested')
+
+parse.add_argument("--out", type=str, default="./accuracy.json")
+args = parse.parse_args()
+
+if __name__ == '__main__':
+
+    data = SingleCharData(64, 64, 3899)
+    data.load_char_map("label_maps/single_char.json").read(args.src_dir).init_indices()
+    main = Main()
+    results = main.infer(data, batch_size=64, ckpt_dir='./ckpts/single_char')
+    labels = data.unmap(data.labels.tolist())
+
+    char_map = {}
+    for result, label in zip(results, labels):
+        # for result, label, img in zip(results, labels, data.images):
+        #     show(img.reshape(img.shape[:2]))
+        if label not in char_map:
+            char_map[label] = {
+                'total': 0,
+                'bingo': 0
+            }
+        char_map[label]['total'] += 1
+        char_map[label]['bingo'] += (1 if result == label else 0)
+
+    acc = {}
+    for char, obj in char_map.items():
+        acc[char] = obj['bingo'] / obj['total']
+    with open(args.out, 'w', encoding='utf-8') as f:
+        json.dump(acc, f, ensure_ascii=False, indent=2)
