6106dfa6ef16a7f4e6362a4d9a9d8565c9470cd6
AssKickerSurfaceBook <121584565@163.com>
2018-11-08 16:02:55 +0800

    new todo: text orientation correcting

---
+++ utils/orientation.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+
+
+def fix_orientation(img):
+    """
+    Text orientation correcting.
+    :param img: a numpy array with shape (h, w) corresponding to a image, in which the text paragraph may be
+                rotated by an angle.
+    :return: the output image, in which the text is horizontally oriented.
+    """
+    # todo 文字倾斜校正
+    pass
