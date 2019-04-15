bfdec5697d51074664725d5c6cd8369715234584
yaolinxia <18860976931@163.com>
2019-04-15 20:55:00 +0800

    args重新指定配置文件

--- args.py
+++ args.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-f = open('configs/single_char.yaml', encoding='utf-8')
+f = open('configs/single_char_3991.yaml', encoding='utf-8')
 # f = open('configs/punctuation_letter_digit.yaml', encoding='utf-8')
 args = yaml.load(f.read())
 f.close()
